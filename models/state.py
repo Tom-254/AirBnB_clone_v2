4013f2be4041582ca86ac9ed05ed28234b079e76
Tom-254 <211mseeratiego@gmail.com>
2022-12-07 22:07:54 +0000

    0x02. AirBnB clone - MySQL

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the review class"""
+from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class Review(BaseModel, Base):
+    """This is the class for Review"""
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='reviews')
+    place = relationship('Place', back_populates='reviews')
