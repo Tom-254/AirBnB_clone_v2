e9675f92620505b17337eac81ca08420f7c8f83c
Tom-254 <211mseeratiego@gmail.com>
2022-12-08 03:54:18 +0000

    0x02. AirBnB clone - MySQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,13 @@
--- create database and grant permission
+-- Creates the database hbnb_test_db with specified paramenters
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Creates user if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- Sets password for user
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Grants privileges to user on database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema .* TO 'hbnb_test'@'localhost';
+-- Grants select privileges to user on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Flush privileges
+FLUSH PRIVILEGES;
